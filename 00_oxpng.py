e6577e377813c630a6148b5d152c67cebf3dd91e
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-16 19:59:06 +0900

    Update 00_oxpng.py

--- 00_oxpng.py
+++ 00_oxpng.py
@@ -68,66 +68,66 @@ if uploaded_file:
     from matplotlib.patches import Rectangle
     #def PlotEach():
     
-        ax = sns.stripplot(x=불합격0['평균등급'], y=불합격0['학과명'], data=불합격0, marker="x",s=14,color='#111111' ,jitter=False, alpha=1, linewidth=1)
+    ax = sns.stripplot(x=불합격0['평균등급'], y=불합격0['학과명'], data=불합격0, marker="x",s=14,color='#111111' ,jitter=False, alpha=1, linewidth=1)
 
 
-        pnts = np.linspace(0, np.pi * 2, 24)
-        circ = np.c_[np.sin(pnts) / 3, -np.cos(pnts) / 3]
-        vert = np.r_[circ, circ[::-1] * .7]
+    pnts = np.linspace(0, np.pi * 2, 24)
+    circ = np.c_[np.sin(pnts) / 3, -np.cos(pnts) / 3]
+    vert = np.r_[circ, circ[::-1] * .7]
 
-        open_circle = mpl.path.Path(vert)
+    open_circle = mpl.path.Path(vert)
 
-        ax = sns.stripplot(x=합격0['평균등급'], y=합격0['학과명'], data=합격0, marker=open_circle,s=17, color='#5954ED', jitter = False)
-
-
-
-
-
-        ax.figure.set_size_inches(15, 23)
-        ax.set_xlim([1,9])
-        ax.set_xticks((1,2,3,4,5,6,7,8,9))
-        ax.set_xticklabels(['1등급','2등급','3등급','4등급','5등급','6등급','7등급','8등급','9등급'])    
-
-
-        ax.set_title('2023학년도 '& choice_column &' 등급 OX 산포도'+'\n', fontsize=25)
-
-        ax.tick_params(right=False, top=True, labelright=False, labeltop=True)  # 모두 True일 경우 x축 2개 y축 2개
-
-        ax.tick_params (axis = 'x', labelsize =12)
-        ax.tick_params (axis = 'y', labelsize =15)
-        ax.set_xlabel('', fontsize=10, color = '#5D5D5D')
-        ax.set_ylabel('', fontsize=10, color = '#5D5D5D')
-        #ax.legend(['O 합격(충원합격포함)' +'\n'+'X 불합격' ],fontsize =11, loc =(0.85,1.03))
+    ax = sns.stripplot(x=합격0['평균등급'], y=합격0['학과명'], data=합격0, marker=open_circle,s=17, color='#5954ED', jitter = False)
 
 
 
-        plt.text(7.60,-0.05,'O' ,fontsize=17, color='#5954ED')
-        plt.text(7.62,0.34,'X' ,fontsize=16, color='#202020')
-        plt.text(7.78,-0.1,'합격(충원합격포함)' ,fontsize=15, color='black')
-        plt.text(7.78,0.32,'불합격' ,fontsize=15, color='black')
-        plt.gca().add_patch(Rectangle((7.55,-0.4),1.4,0.85,linewidth=1,edgecolor='#E4E4E4',facecolor='#E4E4E4')) 
-
-        for i in su:
-            plt.text(1.1,i,'평균: '+ str(shen[int(i-0.1)]) ,fontsize=13, color='#006699') #평균을 집어넣자
-            plt.gca().add_patch(Rectangle((1.08,-0.2+int(i-0.1)),0.6,0.58,linewidth=1,edgecolor='#006699',facecolor='none'))  # 네모칸을 쳐보자
 
 
-        plt.gca().add_patch(Rectangle((1.08,3.8),0.6,0.58,linewidth=1,edgecolor='#006699',facecolor='none'))    
+    ax.figure.set_size_inches(15, 23)
+    ax.set_xlim([1,9])
+    ax.set_xticks((1,2,3,4,5,6,7,8,9))
+    ax.set_xticklabels(['1등급','2등급','3등급','4등급','5등급','6등급','7등급','8등급','9등급'])    
 
 
-        ax = plt.gca()
-        ax.grid(which='major', axis='both', linestyle=':', color='gray')
-        ax.set_facecolor('w')
+    ax.set_title('2023학년도 대진대학교 OX 산포도'+'\n', fontsize=25)
+
+    ax.tick_params(right=False, top=True, labelright=False, labeltop=True)  # 모두 True일 경우 x축 2개 y축 2개
+
+    ax.tick_params (axis = 'x', labelsize =12)
+    ax.tick_params (axis = 'y', labelsize =15)
+    ax.set_xlabel('', fontsize=10, color = '#5D5D5D')
+    ax.set_ylabel('', fontsize=10, color = '#5D5D5D')
+    #ax.legend(['O 합격(충원합격포함)' +'\n'+'X 불합격' ],fontsize =11, loc =(0.85,1.03))
 
 
-        ax.spines["bottom"].set_color("#5E5E5E")
-        ax.spines["left"].set_color("#5E5E5E")
-        ax.spines["top"].set_color("#5E5E5E")
-        ax.spines["right"].set_color("#5E5E5E")
 
-        plt.gcf().subplots_adjust(left=0.2)
+    plt.text(7.60,-0.05,'O' ,fontsize=17, color='#5954ED')
+    plt.text(7.62,0.34,'X' ,fontsize=16, color='#202020')
+    plt.text(7.78,-0.1,'합격(충원합격포함)' ,fontsize=15, color='black')
+    plt.text(7.78,0.32,'불합격' ,fontsize=15, color='black')
+    plt.gca().add_patch(Rectangle((7.55,-0.4),1.4,0.85,linewidth=1,edgecolor='#E4E4E4',facecolor='#E4E4E4')) 
 
-        png = plt.savefig('대진대ox산포도.png')
+    for i in su:
+        plt.text(1.1,i,'평균: '+ str(shen[int(i-0.1)]) ,fontsize=13, color='#006699') #평균을 집어넣자
+        plt.gca().add_patch(Rectangle((1.08,-0.2+int(i-0.1)),0.6,0.58,linewidth=1,edgecolor='#006699',facecolor='none'))  # 네모칸을 쳐보자
+
+
+    plt.gca().add_patch(Rectangle((1.08,3.8),0.6,0.58,linewidth=1,edgecolor='#006699',facecolor='none'))    
+
+
+    ax = plt.gca()
+    ax.grid(which='major', axis='both', linestyle=':', color='gray')
+    ax.set_facecolor('w')
+
+
+    ax.spines["bottom"].set_color("#5E5E5E")
+    ax.spines["left"].set_color("#5E5E5E")
+    ax.spines["top"].set_color("#5E5E5E")
+    ax.spines["right"].set_color("#5E5E5E")
+
+    plt.gcf().subplots_adjust(left=0.2)
+
+    png = plt.savefig('대진대ox산포도.png')
             
                 
     st.pyplot(png)
