6fb3c5ae0b85a9bfbe885e0c36cc1e766959984d
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-16 20:05:52 +0900

    Update 00_oxpng.py

--- 00_oxpng.py
+++ 00_oxpng.py
@@ -12,7 +12,7 @@ from io import StringIO, BytesIO  # Standard Python Module
 
 
 #plt.rc('font', family='NanumBarunGothic')
-plt.rc('axes', unicode_minus=False)
+#plt.rc('axes', unicode_minus=False)
 
 
 
