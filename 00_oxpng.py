7c02061f3592b9edd199c6e6dcee5480fa926d90
DylanM0 <47962132+DylanM0@users.noreply.github.com>
2023-01-18 08:58:00 +0900

    Update 00_oxpng.py

--- 00_oxpng.py
+++ 00_oxpng.py
@@ -95,7 +95,7 @@ if uploaded_file:
 
     
 
-    ax = sns.stripplot(x=불합격0['평균등급'], y=불합격0['학과명'], data=불합격0, marker="x",s=14,color='#111111' ,jitter=False, alpha=1, linewidth=1)
+    ax = sns.stripplot(x=불합격0['평균등급'], y=불합격0['학과명'], data=불합격0, marker="x",s=14,color='#9FAEB7' ,jitter=False, alpha=1, linewidth=1)
 
 
 
